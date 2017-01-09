797955df731151029feb5ed5131fa493e8a2a3d4
sowiecki <nase00@gmail.com>
2017-01-08 18:46:29 -0600

    More memory management

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
+/* eslint no-console:0 */
+/* globals console */
+
+const cluster = require('cluster');
+
 require('babel-register');
 require('babel-polyfill');
-require('./server');
+
+if (cluster.isMaster) {
+  cluster.fork();
+
+  cluster.on('exit', (deadWorker) => {
+    const worker = cluster.fork();
+
+    console.log(`worker ${deadWorker.process.pid} died`);
+    console.log(`worker ${worker.process.pid} born`);
+  });
+} else {
+  require('kiwf')({ maxMemory: 900000000 });
+
+  require('./server');
+}
