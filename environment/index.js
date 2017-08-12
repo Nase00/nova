b1311a54228e0f525a3e4193b4c3c494e209247b
sean <nase00@gmail.com>
2017-08-12 10:16:06 -0500

    Safe file reading

--- environment/index.js
+++ environment/index.js
@@ -1,15 +1,34 @@
 /* eslint import/prefer-default-export:0 */
+/* eslint no-console:0 */
 import path from 'path';
 import { readFileSync } from 'fs';
 
-const readFile = (fileName) => {
+const readJSON = (fileName) => {
   const filePath = path.join(__dirname, fileName);
 
   return JSON.parse(readFileSync(filePath, 'utf8'));
 };
 
-const boards = readFile('../environment/boards.json');
-const raspi = readFile('../environment/raspi.json');
+const getEnv = () => {
+  let boards = {};
+  let raspi = {};
+
+  try {
+    boards = readJSON('../environment/boards.json');
+  } catch (e) {
+    console.log('No boards configuration found.');
+  }
+
+  try {
+    raspi = readJSON('../environment/raspi.json');
+  } catch (e) {
+    console.log('No raspi configuration found.');
+  }
+
+  return { boards, raspi };
+};
+
+const { boards, raspi } = getEnv();
 
 export const defaultEnvironment = {
   boards: { ...boards },
