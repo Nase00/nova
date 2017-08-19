2663a4d395029141eb39b1763158f3987943e383
sowiecki <nase00@gmail.com>
2017-08-18 22:04:55 -0500

    Raspi pixel test

--- server/effects/index.js
+++ server/effects/index.js
@@ -1,9 +1,11 @@
 import cylon from './cylon';
+import cylonR from './cylonR';
 import flash from './flash';
 import rain from './rain';
 
 export default {
   cylon,
+  cylonR,
   flash,
   rain
 };
